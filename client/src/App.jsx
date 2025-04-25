ef526cba0624b19a681985e0b6ac79540c38b411
juiceman1014 <justintran1014@gmail.com>
2025-04-24 23:26:14 -0700

    added frontend support for file uploading

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -19,12 +19,30 @@ function App() {
       }else{
         alert("This message is legit!")
       }
-      
+
     } catch(err){
       console.error("Error submitting message:", err);
       alert("Submission failed!");
     }
   }
+
+  const handleFileUpload = async (event) => {
+    const formData = new FormData();
+    formData.append("file", event.target.files[0]);
+
+    try{
+      const res = await axios.post("http://localhost:8000/scan-file", formData);
+      
+      if(res.data.is_phishing){
+        alert("This message is likely a phishing attempt!")
+      }else{
+        alert("This message is legit!")
+      }
+    } catch(err){
+      console.error(err);
+      alert("failed to scan the file.");
+    }
+  };
 
   return (
     <div className = "flex flex-col justify-evenly items-center h-screen">
@@ -36,6 +54,8 @@ function App() {
         placeholder="Enter your suspicious message"
       />
       <button onClick={handleSubmit}>Submit</button>
+      <div>You can also submit a file for phishing analysis!</div>
+      <input type="file" onChange={handleFileUpload}/>
     </div>
   )
 }
