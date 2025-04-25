b9a0eb411f996bdcd995228765a6a48aa32a2e5c
juiceman1014 <justintran1014@gmail.com>
2025-04-24 23:02:47 -0700

    added basic phishing analysis method

--- server/server.py
+++ server/server.py
@@ -1,15 +1,33 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import re
 
 app = Flask(__name__)
 CORS(app)
 
+def is_phishing(message):
+    msg = message.lower()
+    score = 0
+
+    keywords = ["urgent", "click here", "limited time"]
+    score += sum(kw in msg for kw in keywords)
+
+    if re.search(r"https?://", msg):
+        score += 1
+    
+    if message.count("!") > 3:
+        score += 1
+
+    return score >= 3
+    
 @app.route("/analyze", methods=["POST"])
 def analyze():
     data = request.get_json()
     message = data.get("message", "")
+    phishing = is_phishing(message)
+
     print("Received message:", message)
-    return jsonify({"status": "received", "message":message})
+    return jsonify({"status": "received", "message":message, "is_phishing":phishing})
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
