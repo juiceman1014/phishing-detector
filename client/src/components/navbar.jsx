dbb36fd4955d9f04688a64a55f9e0ef01efa3b44
juiceman1014 <justintran1014@gmail.com>
2025-04-29 00:39:57 -0700

    navbar initial commit

---
+++ client/src/components/navbar.jsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom"
+
+const Navbar = () => {
+    
+    return (
+        <nav>
+            <Link to="/">
+                <button>Home</button>
+            </Link>
+            <Link to="/text">
+                <button>Text Submit</button>
+            </Link>
+            <Link to="/upload">
+                <button>Upload File</button>
+            </Link>
+        </nav>
+    );
+}
+
+export default Navbar;
